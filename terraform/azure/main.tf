ce3d154d16d87326d46be4747b17f6f03a6bc7f8
Juan Mateu <juanma.mateu@gmail.com>
2023-06-06 20:58:34 -0300

    fix subnet

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -28,16 +28,17 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 
-  subnet {
-    name           = "appweb-subnet"
-    address_prefix = "10.0.0.0/24"  
-    private_endpoint_network_policies_enabled = true
-  }
-
   tags = {
     env = "prod"
   }
 }
 
-
+# Define the subnet
+resource "azurerm_subnet" "desafio-subnet" {
+  name                 = "desafio-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  private_endpoint_network_policies_enabled = true
+}
 
