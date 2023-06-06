f6189d0305276270c10860d7986cff379a5db3d2
Juan Mateu <juanma.mateu@gmail.com>
2023-06-06 20:52:38 -0300

    resource group, vnet y subnet

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+
+
+FrontEnd
+vCPU: 1
+Memory: 1GB
+SSD: 20 GB
+Database:
+vCPU: 1
+Memory: 2GB
+SSD: 32GB
--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -15,6 +15,8 @@ provider "azurerm" {
     features {}
 }
 
+
+# resource group "rg" 
 resource "azurerm_resource_group" "rg" {
     name     = "educacionit-devops-rg"
     location = "eastus"
@@ -24,24 +26,18 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.project}-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.2.0.0/20"]
+  address_space       = ["10.0.0.0/16"]
 
   subnet {
-    name           = "default"
-    address_prefix = "10.2.1.0/24"  
+    name           = "appweb-subnet"
+    address_prefix = "10.0.0.0/24"  
+    private_endpoint_network_policies_enabled = true
   }
-
-  subnet {
-    name           = "frontend"
-    address_prefix = "10.2.2.0/24"
-  }
-
-    subnet {
-        name = "backend"
-        address_prefix = "10.2.3.0/24"
-    }
 
   tags = {
     env = "prod"
   }
 }
+
+
+
